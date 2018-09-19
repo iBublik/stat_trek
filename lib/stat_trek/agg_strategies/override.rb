29b2a30a6fbea5c2ff137f5d5a78c298674a2869
Artem Pyankov <artpyankov@gmail.com>
2018-09-19 23:17:41 +0500

    Simplify override strategy

--- lib/stat_trek/agg_strategies/override.rb
+++ lib/stat_trek/agg_strategies/override.rb
@@ -2,11 +2,7 @@ module StatTrek
   module AggStrategies
     class Override < Base
       def call(stats_instance, value)
-        primary_key = stats_instance.class.primary_key
-
-        stats_instance.class
-          .where(primary_key => stats_instance[primary_key])
-          .update_all(field => value)
+        stats_instance.update!(field => value)
       end
     end
   end
